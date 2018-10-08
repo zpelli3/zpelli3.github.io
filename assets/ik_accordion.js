2f1f5ebe49b1d6b337732a4a695ee723c1fc407a
zpelli3 <zpelli3@gmail.com>
2018-10-08 15:22:26 -0400

    add accessibility features to accordion page

--- assets/ik_accordion.js
+++ assets/ik_accordion.js
@@ -33,27 +33,18 @@
 		plugin = this;
 
 		$elem.attr({
-			'id': id,
-      'role': 'region'
+			'id': id
 		}).addClass('ik_accordion');
-
-    this.headers = $elem.children('dt')
-        .attr({'role': 'heading'}); // set heading role for each accordion header
 
 		this.headers = $elem.children('dt').each(function(i, el) {
 			var $me, $btn;
 
 			$me = $(el);
 			$btn = $('<div/>').attr({
-          'id': id + '_btn_' + i,
-          'role': 'button',
-          'aria-controls': id + '_panel_' + i, // associate button with corresponding panel
-          'aria-expanded': false, // toggle expanded state
-          'tabindex': 0 //add keyboard focus
+          'id': id + '_btn_' + i
         })
         .addClass('button')
         .html($me.html())
-        .on('keydown', {'plugin': plugin}, plugin.onKeyDown) // enable keyboard navigation
         .on('click', {'plugin': plugin}, plugin.togglePanel);
 
 			$me.empty().append($btn); // wrap content of each header in an element with role button
@@ -62,16 +53,11 @@
 		this.panels = $elem.children('dd').each(function(i, el) {
 			var $me = $(this), id = $elem.attr('id') + '_panel_' + i;
 			$me.attr({
-				'id': id,
-        'role': 'region', // add role region to each panel
-        'aria-hidden': true, // mark all panels as hidden
-        'tabindex': 0 // add panels into the tab order
+				'id': id
 			});
 		}).hide();
 
 	};
-
-  $elem.attr({'aria-multiselectable': !this.options.autoCollapse}); // define if more than one panel can be expanded
 
 	/**
 	 * Toggles accordion panel.
