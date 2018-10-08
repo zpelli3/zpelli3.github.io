1d4a15ec4c7d2e63226d3c4f19f53bee4468a9fa
zpelli3 <zpelli3@gmail.com>
2018-10-08 15:43:04 -0400

    add accessibility features to accordion page

--- assets/ik_accordion.js
+++ assets/ik_accordion.js
@@ -47,10 +47,15 @@
 
 			$me = $(el);
 			$btn = $('<div/>').attr({
-          'id': id + '_btn_' + i
+          'id': id + '_btn_' + i,
+          'role': 'button',
+          'aria-controls': id + '_panel_' + i, // associate button with corresponding panel
+          'aria-expanded': false, // toggle expanded state
+          'tabindex': 0 //add keyboard focus
         })
         .addClass('button')
         .html($me.html())
+        .on('keydown', {'plugin': plugin}, plugin.onKeyDown) // enable keyboard navigation
         .on('click', {'plugin': plugin}, plugin.togglePanel);
 
 			$me.empty().append($btn); // wrap content of each header in an element with role button
@@ -59,7 +64,10 @@
 		this.panels = $elem.children('dd').each(function(i, el) {
 			var $me = $(this), id = $elem.attr('id') + '_panel_' + i;
 			$me.attr({
-				'id': id
+				'id': id,
+        'role': 'region', // add role region to each panel
+        'aria-hidden': true, // mark all panels as hidden
+        'tabindex': 0 // add panels into the tab order
 			});
 		}).hide();
 
@@ -105,6 +113,51 @@
 
 		}
 	};
+
+  /**
+     * Handles kedown event on header button.
+     *
+     * @param {Object} event - Keyboard event.
+     * @param {object} event.data - Event data.
+     * @param {object} event.data.plugin - Reference to plugin.
+     */
+    Plugin.prototype.onKeyDown = function (event) {
+
+        var $me, $header, plugin, $elem, $current, ind;
+
+        $me = $(event.target);
+        $header = $me.parent('dt');
+        plugin = event.data.plugin;
+        $elem = $(plugin.element);
+
+        switch (event.keyCode) {
+
+            // toggle panel by pressing enter key, or spacebar
+            case ik_utils.keys.enter:
+            case ik_utils.keys.space:
+                event.preventDefault();
+                event.stopPropagation();
+                plugin.togglePanel(event);
+                break;
+
+            // use up arrow to jump to the previous header
+            case ik_utils.keys.up:
+                ind = plugin.headers.index($header);
+                if (ind > 0) {
+                    plugin.headers.eq(--ind).find('.button').focus();
+                }
+                console.log(ind);
+                break;
+
+            // use down arrow to jump to the next header
+            case ik_utils.keys.down:
+                ind = plugin.headers.index($header);
+                if (ind < plugin.headers.length - 1) {
+                    plugin.headers.eq(++ind).find('.button').focus();
+                }
+                break;
+        }
+    };
 
 	$.fn[pluginName] = function ( options ) {
 
